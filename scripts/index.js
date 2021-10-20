f3b678ba696df80a440cc387172de9dc209981d2
Andrey Smiyan <activaid@yandex.ru>
2021-10-20 16:20:36 +0300

    feat: создать события для попап

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const popup = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.popup__close-btn');
+const editButton = document.querySelector('.profile__edit-btn');
+const popupForm = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input_type_name');
+const profileName = document.querySelector('.profile__name');
+const aboutInput = document.querySelector('.popup__input_type_about');
+const profileAbout = document.querySelector('.profile__about');
+
+function openPopup() {
+    popup.classList.add('popup_opened')
+}
+
+nameInput.value = profileName.textContent;
+aboutInput.value = profileAbout.textContent;
+
+function closePopup() {
+    popup.classList.remove('popup_opened')
+}
+
+editButton.addEventListener('click', openPopup);
+
+popupCloseButton.addEventListener('click', closePopup);
+
+function submitForm(event) {
+    event.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = aboutInput.value;
+    closePopup();
+}
+
+popupForm.addEventListener('submit', submitForm);
